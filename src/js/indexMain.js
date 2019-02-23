9e4f8a10a53c4a263ecc061e3a862f630955573b
tony <tony@futunn.com>
2019-02-23 15:48:58 +0800

    下楼层

---
+++ src/js/indexMain.js
@@ -0,0 +1,50 @@
+import GameController from './game_controller';
+import '../css/style.less';
+import '../css/fonts/roboto/RobotoCondensed-Bold.css';
+
+var tips = {
+    fail: '弹簧弹子每次弹跳都会回血哦~',
+    success: '恭喜你成为真男人！赶紧分享给小伙伴吧~'
+};
+
+var preloadImg = function () {
+    var imgList = [
+        '../images/spring-up.png'
+    ];
+    for (var i = 0; i < imgList.length; i++) {
+        var _img = new Image();
+        _img.src = imgList[i];
+    }
+};
+
+$(function () {
+    var gc = new GameController();
+    $('body').show();
+    gc.on('gameover', function (e) {
+        $('title').text('老子' + gc.floorScore + '层！-是男人就下100层');
+        $('.text-score').text(gc.floorScore);
+
+        if (gc.floorScore < 100) {
+            $('.game-over .text-desc').text(tips.fail);
+        } else {
+            $('.game-over .text-desc').text(tips.success);
+        }
+    });
+
+    $('#start-game').on('click', function () {
+        $('.game-intro').hide();
+        $('#game-ct').show();
+        gc.start();
+    });
+
+    $('#restart-game').on('click', function () {
+        $('#game-ct').show();
+        $('.game-over').hide();
+        gc.reRun();
+    });
+
+    $('.share-btn').on('click', function () {
+        alert('点击右上角分享给好友');
+    });
+    preloadImg();
+});
